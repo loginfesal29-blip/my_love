3cfad32a58b2c1662880ed298e200744d4e140dc
loginfesal29-blip <loginfesal29@gmail.com>
2026-02-10 00:03:34 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// 🔐 الباسورد = تاريخ 17 / 1 / 2026
+const correctPassword = "17/1/2026";
+
+function unlock() {
+  const input = document.getElementById("password").value;
+  if (input === correctPassword) {
+    document.getElementById("lock").classList.add("hidden");
+    document.getElementById("content").classList.remove("hidden");
+  } else {
+    document.getElementById("error").innerText = "الباسورد غلط ❤️";
+  }
+}
+
+// ⏳ تاريخ ووقت بداية التعارف
+// 12/28/2025 الساعة 12:39 صباحًا
+const startDate = new Date("2025-12-28T00:39:00");
+
+function updateCounter() {
+  const now = new Date();
+  const diff = now - startDate;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+}
+
+setInterval(updateCounter, 1000);
+updateCounter();
